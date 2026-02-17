dd7c6f2dd9676c4d66d059b7fb7f7c7564565fde
tyrey124 <mrtyrey2005@gmail.com>
2026-02-18 01:53:29 +1000

    fix mypy and ruff errors

--- src/adapters/image_recognition.py
+++ src/adapters/image_recognition.py
@@ -1,38 +1,42 @@
-from src.application.common.image_decoder import ImageRecognitionI
+from typing import cast
+
 from openai import AsyncOpenAI
-import asyncio
+
+from src.application.common.image_recognition import ImageRecognitionI
+
 
 class ImageRecognition(ImageRecognitionI):
-  def __init__(self, api_endpoint: str, api_key: str):
-    self.client = AsyncOpenAI(
-      api_key= api_key,
-      base_url = api_endpoint,
-      )
+    def __init__(self, api_endpoint: str, api_key: str) -> None:
+        self.client = AsyncOpenAI(
+        api_key=api_key,
+        base_url=api_endpoint,
+        )
 
-  async def recognize_text(self, image_url: str) -> str:
-    response = await self.client.chat.completions.create(
-      model="nvidia/nemotron-nano-12b-v2-vl:free",
-      temperature=0,
-      messages=[
-          {
-              "role": "system",
-              "content": [
-                  {"type": "text", "text": (
-                      "Ты — OCR-модель. Считай только текст с изображения. "
-                      "Выводи строго в одной строке, в том же порядке, как на картинке. "
-                      "Обязательно считай все наименования товаров и цену."
-                      "Никаких объяснений, комментариев или домыслов."
-                  )}
-              ]
-          },
-          {
-              "role": "user",
-              "content": [
-                  {"type": "image_url", "image_url": {"url": image_url}}
-              ]
-          }
-      ]
-  )
+    async def recognize_text(self, image_url: str) -> str:
+        response = await self.client.chat.completions.create(
+            model="nvidia/nemotron-nano-12b-v2-vl:free",
+            temperature=0,
+            messages=[
+            {
+                "role": "system",
+                "content": [
+                    {"type": "text", "text": (
+                        "Ты — OCR-модель."
+                        "Считай только текст c изображения."
+                        "Выводи строго в одной строке."
+                        "Сохраняй порядок, как на картинке."
+                        "Обязательно считай все наименования товаров и цену."
+                        "Никаких объяснений, комментариев или домыслов."
+                    )}
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image_url", "image_url": {"url": image_url}}
+                ]
+            }
+        ]
+    )
 
-    text = response.choices[0].message.content
-    return text
+        return cast("str", response.choices[0].message.content)
