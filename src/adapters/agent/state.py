296cbea3033d861d04fff8e19870e3055a76350f
BeaverNotACat <beavernotacat@gmail.com>
2026-04-06 22:09:41 +1000

    💩 Race condition workaround

--- src/adapters/agent/state.py
+++ src/adapters/agent/state.py
@@ -10,6 +10,16 @@ from src.domain.value_objects import UserID
 NoStructuredResponse = type(None)
 
 
+def receipt_reducer(
+    _current_receipt: Receipt, updated_receipt: Receipt
+) -> Receipt:
+    """
+    Agent invokes with max_concurency=1 so race_condition not possible
+    But langchain's LastValue reducer invokes error to prevent races
+    """
+    return updated_receipt
+
+
 class InvokeState(TypedDict):
     messages: Required[
         Annotated[list[AnyMessage | dict[str, Any]], add_messages]
@@ -21,5 +31,5 @@ class InvokeState(TypedDict):
 
 class ReceiptModificationState(AgentState[NoStructuredResponse]):
     current_user_id: UserID
-    receipt: Required[Receipt]
+    receipt: Annotated[Required[Receipt], receipt_reducer]
     users: Required[list[User]]
